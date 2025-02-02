d98ce04570d1f7c1d52f77c7268cb9f3a808fbf8
nayasaff <nayasafwandaba@gmail.com>
2025-02-03 01:31:05 +0200

    Fixed map issue

--- addon/services/movement-tracker.js
+++ addon/services/movement-tracker.js
@@ -155,7 +155,13 @@ export default class MovementTrackerService extends Service {
                 const { event } = output;
 
                 if (event === `${type}.location_changed` || event === `${type}.simulated_location_changed`) {
-                    console.log(output);
+                    output = {
+                        ...output,
+                        location : {
+                            ...output.location,
+                            coordinates: output.location.coordinates.reverse()
+                        }
+                    }
                     eventBuffer.add(output);
                     debug(`Incoming socket event added to buffer: ${event}`);
                 }
